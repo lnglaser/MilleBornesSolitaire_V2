e5b0035d7ce4beee43af931e46576a0d729a3ee9
Leon Glaser <leon.glaser@gmail.com>
2023-10-30 12:06:20 -0400

    add cards to deck, need to refine method

---
+++ card_data.py
@@ -0,0 +1,15 @@
+card_data = []
+
+mileage_25 = 10
+mileage_50 = 10
+mileage_75 = 10
+mileage_100 = 12
+mileage_200 = 4
+flat_tire = 3
+out_of_gas = 3
+accident = 3
+red_light = 5
+spare_tire = 6
+gas = 6
+repairs = 6
+green_light = 14
