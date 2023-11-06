f407dde3b63c7f208eccf9e8b42cf750ad6af746
Leon Glaser <leon.glaser@gmail.com>
2023-11-06 13:09:02 -0500

    bug - deck data not being carried from deck_builder to main

--- deck_builder_2.py
+++ deck_builder_2.py
@@ -7,13 +7,14 @@ class DeckBuilder:
         self.card_data = card_data
         self.deck = {}
 
-        def add_card(self, card_category):
-            for i in card_category["value_and_count"]:
-                for j in range(i[1]):
-                    self.card_id += 1
-                    self.deck[self.card_id] = {"card_type": card_category["card_type"]}
-                    self.deck[self.card_id].update({"value": i[0]})
+    def add_card(self, card_category):
+        for i in card_category["value_and_count"]:
+            for j in range(i[1]):
+                self.card_id += 1
+                self.deck[self.card_id] = {"card_type": card_category["card_type"]}
+                self.deck[self.card_id].update({"value": i[0]})
+                print(f"Test from deck_builder_2: {self.deck[self.card_id]}")
 
-        def shuffle_deck(self, deck):
-            shuffled_deck = list(deck.values())
-            random.shuffle(shuffled_deck)
+    def shuffle_deck(self, deck):
+        shuffled_deck = list(deck.values())
+        random.shuffle(shuffled_deck)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-# import deck_builder_2
 from card_data import card_data
 from deck_builder_2 import DeckBuilder
 
+deck_builder = DeckBuilder(card_data)
+
+# for card_category in card_data:
+#     print(card_category)
+
+deck = []
 for card_category in card_data:
-    print(card_category)
+    new_cards = deck_builder.add_card(card_category=card_category)
+    deck.append(new_cards)
+
+print(deck)
