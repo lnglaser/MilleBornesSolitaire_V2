31240dbfb2fb693d732e8182bdfbf95805687799
Leon Glaser <leon.glaser@gmail.com>
2023-11-13 13:49:06 -0500

    remove unneeded print statement

--- deck_builder_2.py
+++ deck_builder_2.py
@@ -8,13 +8,15 @@ class DeckBuilder:
         # self.deck = {}
         self.all_cards = {}
 
+    # accepts a dictionary with 2 key-value pairs; one is the type of card, the other
+    # is a list of tuples which delineate specific card values and the associated amount of
+    # them present in the game. Adds an item to a hash map per number of cards defined.
     def add_card(self, card_category):
         for i in card_category["value_and_count"]:
             for j in range(i[1]):
                 self.card_id += 1
                 self.all_cards[self.card_id] = {"card_type": card_category["card_type"]}
                 self.all_cards[self.card_id].update({"value": i[0]})
-                print(f"Test from deck_builder_2: {self.all_cards[self.card_id]}")
 
     def shuffle_deck(self, all_cards):
         shuffled_deck = list(all_cards.values())
--- main.py
+++ main.py
@@ -3,20 +3,9 @@ from deck_builder_2 import DeckBuilder
 
 deck_builder = DeckBuilder(card_data)
 
-# for card_category in card_data:
-#     print(card_category)
-
-# deck = []
-# for card_category in card_data:
-#     print(f"card category: {card_category}")
-#     new_cards = deck_builder.add_card(card_category=card_category)
-#     print("main test - new cards")
-#     print(new_cards)
-# deck.append(new_cards)
-
+# calls add_card method from deck_builder_2; loops based on number of items in card_data list
 for card_category in card_data:
     deck = deck_builder.add_card(card_category)
-# deck = deck_builder.add_card(card_data[0])
 
 deck = deck_builder.get_all_cards()
 deck = deck_builder.shuffle_deck(deck)
