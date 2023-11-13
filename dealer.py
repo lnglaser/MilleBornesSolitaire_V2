048e2d6f3a0a2aac563c43437d8c4120c4993baf
Leon Glaser <leon.glaser@gmail.com>
2023-11-13 14:02:01 -0500

    begin dealer component

--- dealer.py
+++ dealer.py
@@ -1,3 +1,13 @@
+# This component will handle (at least) creating and managing players' hands,
+# This will be an improvement over V1 since it can be used for the humand player and
+# the computer player, instead of each getting separate decks.
+
+
 class Dealer:
-    def __init__(self):
-        pass
+    def __init__(self, shuffled_deck):
+        self.deck = shuffled_deck
+        self.hand_size = 6
+
+    def deal_cards(self, deck):
+        for i in range(self.hand_size):
+            print("card dealt")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from card_data import card_data
 from deck_builder_2 import DeckBuilder
+from dealer import Dealer
 
 deck_builder = DeckBuilder(card_data)
+
 
 # calls add_card method from deck_builder_2; loops based on number of items in card_data list
 for card_category in card_data:
@@ -10,3 +12,6 @@ for card_category in card_data:
 deck = deck_builder.get_all_cards()
 deck = deck_builder.shuffle_deck(deck)
 print(f"Main test:\n{deck}")
+
+dealer = Dealer(deck)
+hand = dealer.deal_cards(deck)
