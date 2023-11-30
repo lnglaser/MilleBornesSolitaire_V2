9331d14aec70b8c908cd21d880f90e60a938074f
Leon Glaser <leon.glaser@gmail.com>
2023-11-30 11:50:07 -0500

    restructure card data to make hazard/remedy associations easier

--- card_data.py
+++ card_data.py
@@ -1,41 +1,41 @@
 card_data = [
     {
         "card_type": "Mileage",
-        "value_and_count": [
-            (25, 10),
-            (50, 10),
-            (75, 10),
-            (100, 12),
-            (200, 4),
+        "value_count_matchID": [
+            (25, 10, 0),
+            (50, 10, 0),
+            (75, 10, 0),
+            (100, 12, 0),
+            (200, 4, 0),
         ],
     },
     {
         "card_type": "Hazard",
-        "value_and_count": [
-            ("flat tire", 3),
-            ("out of gas", 3),
-            ("accident", 3),
-            ("speed limit", 4),
-            ("red light", 5),
+        "value_count_matchID": [
+            ("flat tire", 3, 1),
+            ("out of gas", 3, 2),
+            ("accident", 3, 3),
+            ("speed limit", 4, 4),
+            ("red light", 5, 5),
         ],
     },
     {
         "card_type": "Remedy",
-        "value_and_count": [
-            ("spare tire", 6),
-            ("gas", 6),
-            ("repairs", 6),
-            ("end of limit", 6),
-            ("green light", 14),
+        "value_count_matchID": [
+            ("spare tire", 6, 1),
+            ("gas", 6, 2),
+            ("repairs", 6, 3),
+            ("end of limit", 6, 4),
+            ("green light", 14, 5),
         ],
     },
     {
         "card_type": "Safety",
-        "value_and_count": [
-            ("puncture-proof", 1),
-            ("extra tank", 1),
-            ("driving ace", 1),
-            ("right of way", 1),
+        "value_count_matchID": [
+            ("puncture-proof", 1, 1),
+            ("extra tank", 1, 2),
+            ("driving ace", 1, 3),
+            ("right of way", 1, 4),
         ],
     },
 ]
--- deck_builder_2.py
+++ deck_builder_2.py
@@ -12,18 +12,19 @@ class DeckBuilder:
     # is a list of tuples which delineate specific card values and the associated amount of
     # them present in the game. Adds an item to a hash map per number of cards defined.
     def add_card(self, card_category):
-        for i in card_category["value_and_count"]:
+        for i in card_category["value_count_matchID"]:
             for j in range(i[1]):
                 self.card_id += 1
                 self.all_cards[self.card_id] = {"card_type": card_category["card_type"]}
                 self.all_cards[self.card_id].update({"value": i[0]})
+                self.all_cards[self.card_id].update({"match_ID": i[2]})
 
     def shuffle_deck(self, all_cards):
         shuffled_deck = list(all_cards.values())
         random.shuffle(shuffled_deck)
-        # print(f"DB - Shuffle Deck test:\n{shuffled_deck}")
+        print(f"DB - Shuffle Deck test:\n{shuffled_deck}")
         return shuffled_deck
 
     def get_all_cards(self):
-        # print(f"DB - all cards test: {self.all_cards}")
+        print(f"DB - all cards test: {self.all_cards}")
         return self.all_cards
