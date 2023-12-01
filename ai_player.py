55eb9086926e89537592957e4d96a7b08d5d071b
Leon Glaser <leon.glaser@gmail.com>
2023-12-01 13:48:15 -0500

    move player actions to function in main, begin ai component

---
+++ ai_player.py
@@ -0,0 +1,15 @@
+class AI_player:
+    def __init__(self, hand):
+        self.hand = hand
+
+    def ai_miles(self):
+        pass
+
+    def ai_hazard(self):
+        pass
+
+    def ai_remedy(self):
+        pass
+
+    def ai_safety(self):
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from card_data import card_data
 from deck_builder_2 import DeckBuilder
 from dealer import Dealer
+from ai_player import AI_player
 
 deck_builder = DeckBuilder(card_data)
 
@@ -15,7 +16,9 @@ deck = deck_builder.shuffle_deck(deck)
 
 dealer = Dealer(deck)
 
-player = {
+opponent = AI_player
+
+player_info = {
     "hand": [],
     "score": 0,
     "battle_pile": {"card_type": "Hazard", "value": "flat tire", "match_ID": 1},
@@ -23,7 +26,7 @@ player = {
     "safety_pile": [],
 }
 
-opponent = {
+opponent_info = {
     "hand": [],
     "score": 0,
     "battle_pile": {},
@@ -34,75 +37,88 @@ opponent = {
 # May need to add variable for a turn indicator to prevent losing a turn in case player makes a mistake, and
 # to make coup fourre functions possible
 
+# Need to adjust to allow active player to play or discard drawn card as well as cards in hand
+
 # Generate initial hands for player and opponent - retrieve hand size defined in dealer.py, and uses that
 # as range for adding cards to each
 
 for i in range(dealer.hand_size):
-    player["hand"].append(dealer.deal_cards(deck))
-    opponent["hand"].append(dealer.deal_cards(deck))
+    player_info["hand"].append(dealer.deal_cards(deck))
+    opponent_info["hand"].append(dealer.deal_cards(deck))
 
 # Testing print statements
-print(f"Main - player hand:\n{player['hand']}")
-print(f"Main - opponent hand:\n{opponent['hand']}")
+# print(f"Main - player hand:\n{player['hand']}")
+print(f"Main - opponent hand:\n{opponent_info['hand']}")
 print(f"new top card:\n{deck[0]}")
+
 
 # This section will be for establishing and testing game logic - adjust to remove print
 # and input statements once work on frontend begins
-player_move = input("What would you like to do? (play/discard): ")
+def player_turn():
+    player_info["hand"].append(dealer.deal_cards(deck))
+    print(f"Main - player hand:\n{player_info['hand']}")
+    player_move = input("What would you like to do? (play/discard): ")
 
-# Discard a card
-if player_move == "discard":
-    card_number = int(input("Which card do you want to discard? (0-5)"))
-    player["hand"] = dealer.discard(player["hand"][card_number], player["hand"])
-    player["hand"].append(dealer.deal_cards(deck))
-
-# Play a card
-elif player_move == "play":
-    card_number = int(input("Which card do you want to play? (0-5)"))
-
-    # Adding to your score
-    if player["hand"][card_number]["card_type"] == "Mileage":
-        player["score"] = dealer.play_miles(
-            player["hand"][card_number], player["score"]
+    # Discard a card - calls discard and deal_cards methods from dealer.py
+    if player_move == "discard":
+        card_number = int(input("Which card do you want to discard? (0-6)"))
+        player_info["hand"] = dealer.discard(
+            player_info["hand"][card_number], player_info["hand"]
         )
-        player["hand"] = dealer.discard(player["hand"][card_number], player["hand"])
-        player["hand"].append(dealer.deal_cards(deck))
-        print(f"Updated score: {player['score']}")
 
-    # Playing a hazard on opponent
-    elif player["hand"][card_number]["card_type"] == "Hazard":
-        if player["hand"][card_number]["value"] == "speed limit":
-            opponent["speed_pile"] = dealer.play_hazard(
-                player["hand"][card_number],
-                opponent["speed_pile"],
+    # Play a card - checks which card player wants to play
+    elif player_move == "play":
+        card_number = int(input("Which card do you want to play? (0-6)"))
+
+        # Adding to your score
+        if player_info["hand"][card_number]["card_type"] == "Mileage":
+            player_info["score"] = dealer.play_miles(
+                player_info["hand"][card_number], player_info["score"]
             )
-        else:
-            opponent["battle_pile"] = dealer.play_hazard(
-                player["hand"][card_number],
-                opponent["battle_pile"],
+            player_info["hand"] = dealer.discard(
+                player_info["hand"][card_number], player_info["hand"]
+            )
+            player_info["hand"].append(dealer.deal_cards(deck))
+            print(f"Updated score: {player_info['score']}")
+
+        # Playing a hazard on opponent
+        elif player_info["hand"][card_number]["card_type"] == "Hazard":
+            if player_info["hand"][card_number]["value"] == "speed limit":
+                opponent_info["speed_pile"] = dealer.play_hazard(
+                    player_info["hand"][card_number],
+                    opponent_info["speed_pile"],
+                )
+            else:
+                opponent_info["battle_pile"] = dealer.play_hazard(
+                    player_info["hand"][card_number],
+                    opponent_info["battle_pile"],
+                )
+            print(f"Opponent's battle pile: {opponent_info['battle_pile']}")
+            print(f"Opponent's speed pile: {opponent_info['speed_pile']}")
+            player_info["hand"] = dealer.discard(
+                player_info["hand"][card_number], player_info["hand"]
             )
 
-        print(f"Opponent's battle pile: {opponent['battle_pile']}")
-        print(f"Opponent's speed pile: {opponent['speed_pile']}")
-        player["hand"] = dealer.discard(player["hand"][card_number], player["hand"])
-        player["hand"].append(dealer.deal_cards(deck))
+        # Playing a remedy on yourself
+        elif player_info["hand"][card_number]["card_type"] == "Remedy":
+            print(f"Player's battle pile: {player_info['battle_pile']}")
+            player_info["battle_pile"] = dealer.play_remedy(
+                player_info["hand"][card_number], player_info["battle_pile"]
+            )
+            print(print(f"Player's new battle pile: {player_info['battle_pile']}"))
+            print(f"Play remedy - Player's battle pile: {player_info['battle_pile']}")
+            player_info["hand"] = dealer.discard(
+                player_info["hand"][card_number], player_info["hand"]
+            )
 
-    # Playing a remedy on yourself
-    elif player["hand"][card_number]["card_type"] == "Remedy":
-        print(f"Player's battle pile: {player['battle_pile']}")
-        player["battle_pile"] = dealer.play_remedy(
-            player["hand"][card_number], player["battle_pile"]
-        )
-        print(print(f"Player's new battle pile: {player['battle_pile']}"))
-        print(f"Play remedy - Player's battle pile: {player['battle_pile']}")
-        player["hand"] = dealer.discard(player["hand"][card_number], player["hand"])
-        player["hand"].append(dealer.deal_cards(deck))
+        # Playing a safety on yourself
+        elif player_info["hand"][card_number]["card_type"] == "Safety":
+            player_info["safety_pile"] = dealer.play_safety(
+                player_info["hand"][card_number], player_info["safety_pile"]
+            )
+            print(f"Player's safety pile: {player_info['safety_pile']}")
 
-    # Playing a safety on yourself
-    elif player["hand"][card_number]["card_type"] == "Safety":
-        player["safety_pile"] = dealer.play_safety(
-            player["hand"][card_number], player["safety_pile"]
-        )
-        print(f"Player's safety pile: {player['safety_pile']}")
+    print(f"new player hand:\n{player_info['hand']}")
 
-print(f"new player hand:\n{player['hand']}")
+
+player_turn()
