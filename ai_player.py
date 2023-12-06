2171ff127b203a5e55728373646a9cbfbf3c3d0d
Leon Glaser <leon.glaser@gmail.com>
2023-12-06 12:58:38 -0500

    starting on logic statements in ai_turn method for ai_player.py

--- ai_player.py
+++ ai_player.py
@@ -1,9 +1,27 @@
+import random
+
+
 class AI_player:
     def __init__(self, info):
         self.ai_info = info
 
+    # ai_turn will start with a simple choice between playing miles to it's score or playing a hazard
+    # on the player.
     def ai_turn(self):
-        self.ai_miles()
+        print(self.ai_info)
+        if self.ai_info["miles"] == 0 and self.ai_info["battle_pile"] == []:
+            random_number = random.randint(1, 2)
+            print(f"Computer rolled {random_number}")
+            match random_number:
+                case 1:
+                    return "miles"
+                case 2:
+                    return "hazard"
+        elif (
+            self.ai_info["miles"] == 0
+            and self.ai_info["battle_pile"]["card_type"] == "Hazard"
+        ):
+            return "hazard"
 
     def ai_miles(self):
         print("Computer played miles")
